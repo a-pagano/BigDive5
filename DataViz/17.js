b4cc5a9a06a57db69c2a56222f90fe829fd6cbfa
Adriano Pagano <adrianopagano@MacBook-Pro.local>
2016-06-09 13:35:39 +0200

    Add filtering to 17.html/js

--- DataViz/17.js
+++ DataViz/17.js
@@ -26,7 +26,7 @@ d3.csv('athletes_sochi.csv', function(err, data){
         .append('rect')
         .attr('width', 9)
         .attr('x', function(d, i){
-            return i * 10
+            return 420 / (byCountries.length-1) * i 
         })
         .attr('y', function(d, i){
             return 400 - d.values.total
@@ -43,7 +43,7 @@ d3.csv('athletes_sochi.csv', function(err, data){
         byCountries.sort(function(a, b){
             return d3.descending(a.values.total, b.values.total)
             })
-        updateChart()
+        updateChart(byCountries)
     })
     
         
@@ -56,17 +56,38 @@ d3.csv('athletes_sochi.csv', function(err, data){
             return d3.descending(a.values.maxAge, b.values.maxAge)
             })
         
-        updateChart()
+        updateChart(byCountries)
         
         })
     
-    function updateChart(){
+    d3.select('body')
+        .append('button')
+        .text('filter > 30')
+        .on('click', function(){
+        
+        var filtered = byCountries.filter(function(d, i){
+            return d.values.total > 30
+            })
+        
+        updateChart(filtered)
+//        since the filter creates a new collection, passing it to updateChart is needed
+        
+        })
+    
+    
+    function updateChart(data){
         
         d3.select('svg')
             .selectAll('rect')
-            .data(byCountries)
+            .data(data)
             .transition()
             .duration(1000)
+            .attr('width', function(d, i){
+            return 420 / data.length
+            })
+            .attr('x', function(d, i){
+            return 420 / (data.length-1) * i 
+            })
             .attr('height', function(d, i){
                 return d.values.total
             })
@@ -74,6 +95,15 @@ d3.csv('athletes_sochi.csv', function(err, data){
                 return 400 - d.values.total
             })
         
+        d3.select('svg')
+            .selectAll('rect')
+            .data(data)
+            .exit()
+            .transition()
+            .duration(1000)
+            .style('opacity', 0)
+            .remove()
+//        this block is to update the canvas by removing countries that have been filtered out
     }
         
 })
